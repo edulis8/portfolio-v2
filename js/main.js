260e6d83f4e88fe379d1388571f69ab537d59881
edulis8 <eric_broberg@hotmail.com>
2015-09-10 15:39:22 -0700

    clean, white, working

--- js/main.js
+++ js/main.js
@@ -7,13 +7,21 @@ $(document).ready(function() {
 	$('#aboutmelink').click(function() {
 
 		/* Act on the event */
-
+		$('#karmadiv').hide();
+		$('#silentsongdiv').hide();
+		$('#streetfighterdiv').hide();
+		$('#eucliddiv').hide();
+		$('#home').hide();
 		$('#aboutmediv').fadeIn(500);
 	});
 
 	$('#karmalink').click(function() {
 		/* Act on the event */
 		$('#aboutmediv').hide();
+		$('#eucliddiv').hide();
+		$('#streetfighterdiv').hide();
+		$('#silentsongdiv').hide();
+		$('#home').hide();
 		$('#karmadiv').fadeIn(500);
 	});
 
@@ -21,6 +29,9 @@ $(document).ready(function() {
 		/* Act on the event */
 		$('#aboutmediv').hide();
 		$('#karmadiv').hide();
+		$('#eucliddiv').hide();
+		$('#streetfighterdiv').hide();
+		$('#home').hide();
 		$('#silentsongdiv').fadeIn(500);
 	});
 
@@ -29,8 +40,32 @@ $(document).ready(function() {
 		$('#aboutmediv').hide();
 		$('#karmadiv').hide();
 		$('#silentsongdiv').hide();
+		$('#eucliddiv').hide();
+		$('#home').hide();
 		$('#streetfighterdiv').fadeIn(500);
 	});
+
+	$('#euclidlink').click(function() {
+		/* Act on the event */
+		$('#aboutmediv').hide();
+		$('#karmadiv').hide();
+		$('#silentsongdiv').hide();
+		$('#streetfighterdiv').hide();
+		$('#home').hide();
+		$('#eucliddiv').fadeIn(500);
+	});
+
+	$('h1').click(function() {
+		/* Act on the event */
+		$('#aboutmediv').hide();
+		$('#karmadiv').hide();
+		$('#silentsongdiv').hide();
+		$('#streetfighterdiv').hide();
+		$('#eucliddiv').hide();
+		$('#home').fadeIn(500);
+
+
+		});
 
 });
 
