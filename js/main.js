e20fb45d87029a71d7bd0e5b20b94393b6dc0fd9
edulis8 <eric_broberg@hotmail.com>
2015-09-10 14:25:45 -0700

    adding divs/images

--- js/main.js
+++ js/main.js
@@ -5,8 +5,31 @@ $(document).ready(function() {
 	}, 1500);
 
 	$('#aboutmelink').click(function() {
+
 		/* Act on the event */
+
 		$('#aboutmediv').fadeIn(500);
+	});
+
+	$('#karmalink').click(function() {
+		/* Act on the event */
+		$('#aboutmediv').hide();
+		$('#karmadiv').fadeIn(500);
+	});
+
+	$('#silentsonglink').click(function() {
+		/* Act on the event */
+		$('#aboutmediv').hide();
+		$('#karmadiv').hide();
+		$('#silentsongdiv').fadeIn(500);
+	});
+
+	$('#streetfighterlink').click(function() {
+		/* Act on the event */
+		$('#aboutmediv').hide();
+		$('#karmadiv').hide();
+		$('#silentsongdiv').hide();
+		$('#streetfighterdiv').fadeIn(500);
 	});
 
 });
